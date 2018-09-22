e75fad78e22b1704a28a0da0ce85dd6739ce3615
Igor Pereira Gomes <ipgmax@gmail.com>
2018-09-22 05:41:32 -0300

    qwee

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ import glob
 import matplotlib.image as mpimg
 from scipy import misc
 import numpy as np
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -13,14 +14,38 @@ vmcorreto = ""
 vmchecado = ""
 resultado = ""
 
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['jpg'])
 
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def show_index():
+	if request.method == 'POST':
+			# check if the post request has the file part
+			if 'file' not in request.files:
+				flash('No file part')
+				return redirect(request.url)
+			file = request.files['file']
+			# if user does not select file, browser also
+			# submit an empty part without filename
+			if file.filename == '':
+				flash('No selected file')
+				return redirect(request.url)
+			if file and allowed_file(file.filename):
+				filename = secure_filename(file.filename)
+				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+				return redirect(url_for('uploaded_file',
+										filename=filename))
     full_filename = "/static/MC_03_2_jpg.jpg"
-    return render_template("index.html", user_image = full_filename)
+    return render_template("upload.html")
 	
 @app.route('/match/<fname>')
 def match_fname(fname):
-    full_filename = "/static/"+fname+".jpg"
-	vmcorreto = open("static/"+fname+".jpg","rb").read()
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], fname+".jpg")
+	#vmcorreto = open(full_filename,"rb").read()
     return render_template("index.html", user_image = full_filename)

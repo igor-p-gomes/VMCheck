adecd2d582c6229609c16a4cb53bddb381a30c0b
Igor Pereira Gomes <ipgmax@gmail.com>
2018-09-22 06:22:41 -0300

    nodir

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(filename)
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     return '''
